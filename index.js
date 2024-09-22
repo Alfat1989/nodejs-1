b86648563bde5c19fb8e35da4997414d1b006333
alfat <gilmalf89@gmail.com>
2024-09-22 23:08:40 +0500

    init project

---
+++ index.js
@@ -0,0 +1,5 @@
+const fs = require('fs/promises')
+const params = require('./index2')
+
+console.log(fs)
+console.log(params)
---
+++ index2.js
@@ -0,0 +1,6 @@
+const colors = ['red', 'green', 'blue']
+const sizes = [21, 22, 23]
+
+const params = { colors, sizes }
+
+module.exports = params
